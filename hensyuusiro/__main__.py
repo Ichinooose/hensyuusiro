2d267faa9fb5ccd28914eb3f4e15fa5256faded9
Ichinooose <ichinooooooose@gmail.com>
2022-01-13 23:31:18 +0900

    1.1 Release!!

--- hensyuusiro/__main__.py
+++ hensyuusiro/__main__.py
@@ -12,22 +12,23 @@ from lightbulb import *
 from hensyuusiro import *
 
 pretime_dict = {}
-client = lightbulb.BotApp(token=R_TOKEN, prefix="!")
+client = lightbulb.BotApp(token=T_TOKEN, prefix="!")
 
-
+CHANNEL_ID = T_EDITING_CHANNEL_ID
+voice_channel_id = T_VOICE_EDITING_CHANNEL_ID
 
 
 @client.listen()
 async def voiceevent(event: hikari.VoiceStateUpdateEvent) -> None:
     try:
-        if event.state.channel_id == R_VOICE_EDITING_CHANNEL_ID or event.old_state.channel_id == R_VOICE_EDITING_CHANNEL_ID:
+        if event.state.channel_id == int(voice_channel_id) or event.old_state.channel_id == int(voice_channel_id):
             user_id = str(event.state.user_id)
             file = "../hensyuusiro/user/" + user_id + ".yml"
             data = dict(
                 Username=str(event.state.member.username),
                 Total_Editing_Time=0,
                 Total_Video_Time=0,
-                Start_Video_Time="00:00:00",
+                Start_Video_Time="99:99:99",
                 End_Video_Time="00:00:00"
 
             )
@@ -58,8 +59,8 @@ async def voiceevent(event: hikari.VoiceStateUpdateEvent) -> None:
                     Username=str(event.state.member.username),
                     Total_Editing_Time=total_editing_time,
                     Total_Video_Time=config["Total_Video_Time"],
-                    Start_Video_Time="00:00:00",
-                    End_Video_Time="00:00:00"
+                    Start_Video_Time="99:99:99",
+                    End_Video_Time=config["End_Video_Time"]
 
                 )
                 with open(file, "w") as filer:
@@ -78,8 +79,9 @@ async def voiceevent(event: hikari.VoiceStateUpdateEvent) -> None:
                 embed.add_field(name="累計編集時間",
                                 value=str(total_editing_time_hours) + "時" + str(total_editing_time_minutes) + "分" + str(total_editing_time_seconds) + "秒")
 
-                await (await client.rest.fetch_channel(R_EDITING_CHANNEL_ID)).send(embed=embed)
+                await (await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
     except AttributeError:
+        print("error")
         pass
 
 
@@ -89,8 +91,7 @@ async def voiceevent(event: hikari.VoiceStateUpdateEvent) -> None:
 @lightbulb.implements(lightbulb.PrefixCommand)
 async def video_time_start(ctx: lightbulb.Context) -> None:
     try:
-        if ctx.get_guild().get_voice_state(ctx.user).channel_id == R_VOICE_EDITING_CHANNEL_ID:
-
+        if ctx.get_guild().get_voice_state(ctx.user).channel_id == voice_channel_id:
             print(ctx.options.start)
             print(type(ctx.options.start))
             old_hours = ctx.options.start[0:2]
@@ -129,22 +130,77 @@ async def video_time_start(ctx: lightbulb.Context) -> None:
                         with open(file, "w") as filer:
                             filer.write(yaml.dump(data, default_flow_style=False, allow_unicode=True))
 
-                    await (await client.rest.fetch_channel(R_EDITING_CHANNEL_ID)).send(embed=embed)
+                    await (await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
                 except ValueError:
                     await ctx.respond("おっぱいがいっぱい")
     except AttributeError:
         embed = hikari.Embed(color="b22222")
         embed.add_field(name="実行されませんでした。", value="ボイスチャンネルに接続してから実行してください。")
-        await (await client.rest.fetch_channel(R_EDITING_CHANNEL_ID)).send(embed=embed)
+        await (await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
 
 
 @client.command()
 @lightbulb.decorators.option("end", "text to repeat", modifier=commands.OptionModifier.CONSUME_REST)
 @lightbulb.command("end", "動画時間を記録します。", aliases="e")
 @lightbulb.implements(lightbulb.PrefixCommand)
-async def video_time_end(ctx: lightbulb.Context) -> None:
-    try:
-        if ctx.get_guild().get_voice_state(ctx.user).channel_id == R_VOICE_EDITING_CHANNEL_ID:
+async def video_time_end(ctx: lightbulb.Context,) -> None:
+    file = "../hensyuusiro/user/" + str(ctx.member.user.id) + ".yml"
+    with open(file, "r") as yml:
+        config = yaml.safe_load(yml)
+    if int(config["Start_Video_Time"][0:2]) <= int(ctx.options.end[0:2]):
+        try:
+            if ctx.get_guild().get_voice_state(ctx.user).channel_id == voice_channel_id:
+                print(ctx.options.end)
+                print(type(ctx.options.end))
+                new_hours = ctx.options.end[0:2]
+                new_minutes = ctx.options.end[3:5]
+                new_seconds = ctx.options.end[6:8]
+                if type(new_hours) and type(new_minutes) and type(new_seconds) != str:
+                    pass
+                else:
+                    try:
+                        hours = int(new_hours) * 3600
+                        minutes = int(new_minutes) * 60
+                        seconds = int(new_seconds)
+                        total_new_video_time = hours + minutes + seconds
+                        embed = hikari.Embed(color="00fa9a")
+                        user = ctx.user.username
+                        embed.set_author(name=user + "の動画記録", icon=ctx.member.user.avatar_url)
+                        embed.add_field(name="動画時間を記録し終わりました。", value="終了動画時間：" +
+                                                                      str(new_hours) + "時" +
+                                                                      str(new_minutes) + "分" +
+                                                                      str(new_seconds) + "秒")
+                        file = "../hensyuusiro/user/" + str(ctx.member.user.id) + ".yml"
+                        with open(file, "r") as yml:
+                            config = yaml.safe_load(yml)
+                            total_old_video_time = int(config["Start_Video_Time"][0:2]) * 3600 + int(
+                                config["Start_Video_Time"][3:5]) * 60 + int(config["Start_Video_Time"][6:8])
+                            # var_hours = hours - int(config["Start_Video_Time"][0:2])
+                            # var_minutes = minutes - int(config["Start_Video_Time"][3:5])
+                            # var_seconds = seconds- int(config["Start_Video_Time"][6:8])
+                            dif_total_video_time = total_new_video_time - total_old_video_time
+                            dif_hours = dif_total_video_time // 3600
+                            dif_minutes = (dif_total_video_time - dif_hours * 3600) // 60
+                            dif_seconds = (dif_total_video_time - dif_hours * 3600) % 60
+                            total_video_time = dif_total_video_time + config["Total_Video_Time"]
+                        with open(file, "r") as ymla:
+                            config = yaml.safe_load(ymla)
+                            data = dict(
+                                Username=config["Username"],
+                                Total_Editing_Time=config["Total_Editing_Time"],
+                                Total_Video_Time=total_video_time,
+                                Start_Video_Time=config["Start_Video_Time"],
+                                End_Video_Time=str(ctx.options.end)
+
+                            )
+                            with open(file, "w") as filer:
+                                filer.write(yaml.dump(data, default_flow_style=False, allow_unicode=True))
+                            embed.add_field(name="動画編集時間",
+                                            value=str(dif_hours) + "時" + str(dif_minutes) + "分" + str(dif_seconds) + "秒")
+                            await (await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
+                    except ValueError:
+                        await ctx.respond("おっぱいがいっぱい")
+        except AttributeError:
             print(ctx.get_guild().get_voice_state(ctx.user))
             print(ctx.options.end)
             print(type(ctx.options.end))
@@ -193,61 +249,13 @@ async def video_time_end(ctx: lightbulb.Context) -> None:
                             filer.write(yaml.dump(data, default_flow_style=False, allow_unicode=True))
                         embed.add_field(name="動画編集時間",
                                         value=str(dif_hours) + "時" + str(dif_minutes) + "分" + str(dif_seconds) + "秒")
-                        await (await client.rest.fetch_channel(R_EDITING_CHANNEL_ID)).send(embed=embed)
+                        await (await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
                 except ValueError:
                     await ctx.respond("おっぱいがいっぱい")
-    except AttributeError:
-        print(ctx.get_guild().get_voice_state(ctx.user))
-        print(ctx.options.end)
-        print(type(ctx.options.end))
-        new_hours = ctx.options.end[0:2]
-        new_minutes = ctx.options.end[3:5]
-        new_seconds = ctx.options.end[6:8]
-        if type(new_hours) and type(new_minutes) and type(new_seconds) != str:
-            pass
-        else:
-            try:
-                hours = int(new_hours) * 3600
-                minutes = int(new_minutes) * 60
-                seconds = int(new_seconds)
-                total_new_video_time = hours + minutes + seconds
-                embed = hikari.Embed(color="00fa9a")
-                user = ctx.user.username
-                embed.set_author(name=user + "の動画記録", icon=ctx.member.user.avatar_url)
-                embed.add_field(name="動画時間を記録し終わりました。", value="終了動画時間：" +
-                                                              str(new_hours) + "時" +
-                                                              str(new_minutes) + "分" +
-                                                              str(new_seconds) + "秒")
-                file = "../hensyuusiro/user/" + str(ctx.member.user.id) + ".yml"
-                with open(file, "r") as yml:
-                    config = yaml.safe_load(yml)
-                    total_old_video_time = int(config["Start_Video_Time"][0:2]) * 3600 + int(
-                        config["Start_Video_Time"][3:5]) * 60 + int(config["Start_Video_Time"][6:8])
-                    # var_hours = hours - int(config["Start_Video_Time"][0:2])
-                    # var_minutes = minutes - int(config["Start_Video_Time"][3:5])
-                    # var_seconds = seconds- int(config["Start_Video_Time"][6:8])
-                    dif_total_video_time = total_new_video_time - total_old_video_time
-                    dif_hours = dif_total_video_time // 3600
-                    dif_minutes = (dif_total_video_time - dif_hours * 3600) // 60
-                    dif_seconds = (dif_total_video_time - dif_hours * 3600) % 60
-                    total_video_time = dif_total_video_time + config["Total_Video_Time"]
-                with open(file, "r") as ymla:
-                    config = yaml.safe_load(ymla)
-                    data = dict(
-                        Username=config["Username"],
-                        Total_Editing_Time=config["Total_Editing_Time"],
-                        Total_Video_Time=total_video_time,
-                        Start_Video_Time=config["Start_Video_Time"],
-                        End_Video_Time=str(ctx.options.end)
-
-                    )
-                    with open(file, "w") as filer:
-                        filer.write(yaml.dump(data, default_flow_style=False, allow_unicode=True))
-                    embed.add_field(name="動画編集時間",
-                                    value=str(dif_hours) + "時" + str(dif_minutes) + "分" + str(dif_seconds) + "秒")
-                    await (await client.rest.fetch_channel(R_EDITING_CHANNEL_ID)).send(embed=embed)
-            except ValueError:
-                await ctx.respond("おっぱいがいっぱい")
+    else:
+        embed = hikari.Embed(color="b22222")
+        embed.add_field(name="実行されませんでした。", value="限界値に達しているかもしくは開始コマンド入力がされてません。")
+        await (await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
 
 
 @client.command()
@@ -271,6 +279,6 @@ async def stats(ctx: lightbulb.Context) -> None:
         embed.add_field(name="累計動画編集時間",
                         value=str(Total_Video_Time_Hours) + "時" + str(Total_Video_Time_Minutes) + "分" + str(
                             Total_Video_Time_Seconds) + "秒")
-        await(await client.rest.fetch_channel(R_EDITING_CHANNEL_ID)).send(embed=embed)
+        await(await client.rest.fetch_channel(CHANNEL_ID)).send(embed=embed)
 
 client.run()
